7d3a4492510fbc6e2b631a17acb1917899470159
Callum Lees <hi@c-l.dev>
2021-12-18 15:22:44 +0000

    Added and imported BlogPage.vue component to the view/Blog.vue

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="blog">
-    <div class="flex -m-20 min-h-screen mx-auto text-center justify-center items-center">
-      <h1 class="text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-pink-500">
-        This is the blog page
-      </h1>
-    </div>
+    <BlogPage />
   </div>
 </template>
+
+<script>
+import BlogPage from "@/components/BlogPage.vue";
+
+export default {
+  name: "Blog",
+
+  components: {
+    BlogPage,
+  },
+};
+</script>
