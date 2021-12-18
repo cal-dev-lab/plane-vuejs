b8c8a18b119eb2778a90f939db7bf918522bf535
Callum Lees <hi@c-l.dev>
2021-12-18 16:51:58 +0000

    Removed script from Blog.vue

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,17 +1,9 @@
 <template>
   <div class="blog">
-    <BlogPage />
+    <div class="flex -m-20 min-h-screen mx-auto text-center justify-center items-center">
+      <h1 class="text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-pink-500">
+        This is the blog page
+      </h1>
+    </div>
   </div>
 </template>
-
-<script>
-import BlogPage from "@/components/BlogPage.vue";
-
-export default {
-  name: "Blog",
-
-  components: {
-    BlogPage,
-  },
-};
-</script>
