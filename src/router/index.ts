7f7514d5f574039998dd3fc99bf3a1e3d9173e91
Callum Lees <hi@c-l.dev>
2021-12-18 16:51:31 +0000

    Moved placement of route in /router/index.ts to match NavBar.vue layout

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,17 +8,17 @@ const routes: Array<RouteRecordRaw> = [
     component: Home,
   },
   {
+    path: "/planes",
+    name: "Planes",
+    component: () => import(/* webpackChunkName: "planes" */ "../views/Planes.vue"),
+  },
+  {
     path: "/about",
     name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-  {
-    path: "/planes",
-    name: "Planes",
-    component: () => import(/* webpackChunkName: "planes" */ "../views/Planes.vue"),
   },
   {
     path: "/blog",
