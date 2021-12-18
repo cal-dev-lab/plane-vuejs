c6fd5878261042c12db68a46a585fe377087741d
Callum Lees <hi@c-l.dev>
2021-12-18 16:52:25 +0000

    Imported PlanesPage.vue into /views/Plane.vue

--- src/views/Planes.vue
+++ src/views/Planes.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="planes">
-    <div class="flex -m-20 min-h-screen mx-auto text-center justify-center items-center">
-      <h1 class="text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-pink-500">
-        This is the plane photos page
-      </h1>
-    </div>
+    <PlanesPage />
   </div>
 </template>
+
+<script>
+import PlanesPage from "@/components/PlanesPage.vue";
+
+export default {
+  name: "Planes",
+
+  components: {
+    PlanesPage,
+  },
+};
+</script>
